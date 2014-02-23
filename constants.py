13deb11d8d4afafc4e14b151eac1f17b67dcfd6a
anand-ubuntu <aoracle@gmail.com>
2014-02-23 00:58:51 -0800

    Adding constants file

---
+++ constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+amex_table = 'raw_stocks_amex'
+nyse_table = 'raw_stocks_nyse'
+nasdaq_table = 'raw_stocks_nasdaq'
+opra_table = 'raw_option'
+filepath =  '/stocks/raw_data/download'
+archivepath = '/stocks/raw_data/archive'
+ftp_source = '/'
+ftp_username = 'amahajan1981'
+ftp_password = 'gurukul1'
+ftp_site = 'ftp.eoddata.com'
+log_dir =  '/stocks/raw_data/log/'
